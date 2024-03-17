5de2d957fe5c5d8fb82e8448bef0d181d17ad6d8
antelman107 <anton_elmanov@epam.com>
2024-03-17 14:21:21 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/antelman107/vercel_local_go
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+import "github.com/antelman107/vercel_local_go/api"
+
+func main() {
+	http.HandleFunc(
+		"/",
+		api.Handler,
+	)
+
+	if err := http.ListenAndServe(
+		":8080",
+		nil,
+	); err != nil {
+		panic(err)
+	}
+}
