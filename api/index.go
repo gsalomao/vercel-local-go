9e50eec77c37f8621f0d9aa9bf6d2b55e58de9a4
antelman107 <anton_elmanov@epam.com>
2024-07-02 15:44:23 +0300

    output env variables

--- api/index.go
+++ api/index.go
@@ -3,11 +3,16 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(
 		w,
-		"<h1>Hello from Go!</h1>",
+		"<h1>Hello from Go!</h1><br/>",
 	)
+
+	for _, v := range os.Environ() {
+		fmt.Fprintln(w, v+"<br/>")
+	}
 }
