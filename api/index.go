6d36a2ac8d634feb36cab4865213ea25b4c63459
Gustavo Salomão <gsalomao.eng@gmail.com>
2025-09-25 11:06:33 +0100

    Add newline to console output in Handler function

--- api/index.go
+++ api/index.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Hello from Go!")
+	fmt.Printf("Hello from Go!\n")
 	fmt.Fprintf(
 		w,
 		"<h1>Hello from Go!</h1><br/>",
