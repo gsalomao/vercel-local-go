a9c2d258798e37d21aba17d4ba4cbf5dff987ba6
Gustavo Salomão <gsalomao.eng@gmail.com>
2025-09-25 11:14:17 +0100

    Modify Handler to log environment variables
    Updated logging to print environment variables to console.

--- api/index.go
+++ api/index.go
@@ -7,15 +7,13 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Hello from Go!\n")
 	fmt.Fprintf(
 		w,
 		"<h1>Hello from Go!</h1><br/>",
 	)
 
+	fmt.Printf("Hello from Go!\n")
 	for _, v := range os.Environ() {
-
-		fmt.Fprintln(w, v+"<br/>")
+		fmt.Printf("%v\n", v)
 	}
-
 }
